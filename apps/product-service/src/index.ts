b65797f7ea8e07a78e856d85982c8596cac9f681
Aarsh Singh <starfire9599@gmail.com>
2026-07-30 20:22:26 +0530

    feat(product-service): implement Clerk authentication middleware for Express

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response} from "express"
 import cors from "cors";
 import { clerkMiddleware, getAuth } from "@clerk/express";
+import { shouldBeUser } from "./middleware/authMiddleware.js";
 
 const app = express();
 
@@ -20,15 +21,8 @@ app.get("/health", (req: Request, res: Response) => {
   })
 })
 
-app.get("/test", (req, res) => {
-  const auth = getAuth(req);
-  const userId = auth.userId;
-
-  if(!userId){
-    return res.json({ message: "Not logged in" });
-  }
-
-  res.json({ message: "Product service authenticated" });
+app.get("/test", shouldBeUser, (req, res) => {
+  res.json({ message: "Product service authenticated", userId: req.userId });
 });
 
 app.listen(8000, () => {
