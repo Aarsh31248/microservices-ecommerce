62ca3d47adb26d447bb2fb300a8742fef598b76a
Aarsh Singh <starfire9599@gmail.com>
2026-08-02 23:37:57 +0530

    feat(product-service):initialize Kafka producer and consumer lifecycle on startup

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,9 +1,10 @@
-import express, { NextFunction, Request, Response} from "express"
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { clerkMiddleware, getAuth } from "@clerk/express";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
 import productRouter from "./routes/product.route";
 import categoryRouter from "./routes/category.route";
+import { consumer, producer } from "./utils/kafka.js";
 
 const app = express();
 
@@ -13,7 +14,6 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(clerkMiddleware());
 
@@ -21,9 +21,9 @@ app.get("/health", (req: Request, res: Response) => {
   return res.status(200).json({
     status: "ok",
     uptime: process.uptime(),
-    timestamp: Date.now()
-  })
-})
+    timestamp: Date.now(),
+  });
+});
 
 app.get("/test", shouldBeUser, (req, res) => {
   res.json({ message: "Product service authenticated", userId: req.userId });
@@ -36,9 +36,19 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
   return res
     .status(err.status || 500)
-    .json({ message: err.message || "Internal Server Error!" });
+    .json({ message: err.message || "Inter Server Error!" });
 });
 
-app.listen(8000, () => {
-  console.log("Product service is running on port 8000");
-});
+const start = async () => {
+  try {
+    Promise.all([await producer.connect(), await consumer.connect()]);
+    app.listen(8000, () => {
+      console.log("Product service is running on 8000");
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start()
