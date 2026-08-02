83ce3fa086440f4630ef13d8641231df3a74f1cd
Aarsh Singh <starfire9599@gmail.com>
2026-08-03 00:07:20 +0530

    feat(order-service): register payment.successful subscription and wire up boot lifecycle

--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -3,6 +3,8 @@ import Clerk from "@clerk/fastify";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
 import { connectOrderDB } from "@repo/order-db";
 import { orderRoute } from "./routes/order.js";
+import { consumer, producer } from "./utils/kafka.js";
+import { runKafkaSubscriptions } from "./utils/subscriptions.js";
 
 const fastify = Fastify();
 
@@ -12,8 +14,8 @@ fastify.get("/health", (request, reply) => {
   return reply.status(200).send({
     status: "ok",
     uptime: process.uptime(),
-    timestamp: Date.now()
-  })
+    timestamp: Date.now(),
+  });
 });
 
 fastify.get("/test", { preHandler: shouldBeUser }, (request, reply) => {
@@ -27,7 +29,12 @@ fastify.register(orderRoute);
 
 const start = async () => {
   try {
-    await connectOrderDB(),
+    Promise.all([
+      await connectOrderDB(),
+      await producer.connect(),
+      await consumer.connect(),
+    ]);
+    await runKafkaSubscriptions();
     await fastify.listen({ port: 8001 });
     console.log("Order service is running on port 8001");
   } catch (err) {
