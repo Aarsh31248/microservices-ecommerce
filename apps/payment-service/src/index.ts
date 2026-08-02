4af69beaf352c81f5ecdfa113729d932e813dae1
Aarsh Singh <starfire9599@gmail.com>
2026-08-02 23:53:49 +0530

    feat(payment-service):dispatch payment.successful event via Kafka upon checkout completion

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,22 +1,24 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 import { clerkMiddleware } from "@hono/clerk-auth";
 import sessionRoute from "./routes/session.route.js";
 import { cors } from "hono/cors";
 import webhookRoute from "./routes/webhooks.route.js";
+import { consumer, producer } from "./utils/kafka.js";
+import { runKafkaSubscriptions } from "./utils/subscriptions.js";
 
-const app = new Hono()
+const app = new Hono();
 
 app.use("*", clerkMiddleware());
 app.use("*", cors({ origin: ["http://localhost:3002"] }));
 
-app.get('/health', (c) => {
+app.get("/health", (c) => {
   return c.json({
     status: "ok",
     uptime: process.uptime(),
-    timestamp: Date.now()
-  })
-})
+    timestamp: Date.now(),
+  });
+});
 
 app.route("/sessions", sessionRoute);
 app.route("/webhooks", webhookRoute);
@@ -44,6 +46,8 @@ app.route("/webhooks", webhookRoute);
 
 const start = async () => {
   try {
+    Promise.all([await producer.connect(), await consumer.connect()]);
+    await runKafkaSubscriptions();
     serve(
       {
         fetch: app.fetch,
@@ -51,7 +55,7 @@ const start = async () => {
       },
       (info) => {
         console.log(`Payment service is running on port 8002`);
-      }
+      },
     );
   } catch (error) {
     console.log(error);
